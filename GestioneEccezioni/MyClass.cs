2d1f5c7ded5aea2d3c7b1d99dc44ca72c70955cb
Roberto Spagliccia <roberto.spagliccia@gmail.com>
2019-10-29 18:13:58 +0100

    Add project files.

---
+++ GestioneEccezioni/MyClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneEccezioni
+{
+    class MyClass
+    {
+        public int Età { get; private set; }
+
+        /// <summary>
+        /// Imposta l'età della persona
+        /// </summary>
+        /// <param name="età">Descrizione parametro</param>
+        /// <exception cref="CustomException">Una custom exception contenente l'eccezione reale</exception> 
+        /// <seealso cref="SetEtà2"></seealso>
+        public void SetEtà (string età)
+        {
+            try
+            {
+                Età = Int32.Parse(età);
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new CustomException("Il valore inserito è vuoto, inserire un valore corretto", ex);
+            }
+            catch (OverflowException ex)
+            {
+                throw new CustomException("Il valore inserito è troppo grande o troppo piccolo, inserire un valore corretto", ex);
+            }
+            catch (FormatException ex)
+            {
+                throw new CustomException("Il valore inserito non è numerico, inserire un valore corretto", ex);
+            }
+            catch
+            {
+                throw new CustomException("C'è stato un problema.");
+            }
+        }
+    }
+}
---
+++ GestioneEccezioni/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneEccezioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass m = new MyClass();
+            Console.WriteLine("Per favore inserisci l'età");
+
+            string a = "pippo";
+            a = "pluto";
+
+            try
+            {
+                string età = Console.ReadLine();
+                m.SetEtà(età);
+                Console.WriteLine("L'età inserità è: " + m.Età);
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine("C'è stato un problema.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Dettagli tecnici: " + ex.InnerException.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Il valore inserito non è corretto");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Grazie per aver utilizzato il nostro software");
+            }
+
+            //Console.WriteLine("Inserisci un'età");
+
+            //MyClass m = new MyClass();
+            //try
+            //{
+            //    m.SetEtà(Console.ReadLine());
+            //    Console.WriteLine("L'età inserità è: " + m.Età);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Grazie per aver utilizzato il nostro software");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
